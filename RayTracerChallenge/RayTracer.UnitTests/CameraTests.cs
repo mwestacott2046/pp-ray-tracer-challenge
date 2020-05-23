0583c46490189da056e09807dc9ba49acc7fc632
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2020-05-23 21:32:33 +0100

    Add Camera and Example Scene

---
+++ RayTracerChallenge/RayTracer.UnitTests/CameraTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace RayTracer.UnitTests
+{
+    [TestFixture]
+    public class CameraTests
+    {
+        [Test]
+        public void ConstructingACamera()
+        {
+            var hSize = 160;
+            var vSize = 120;
+            var fieldOfView = Math.PI / 2;
+
+            var camera = new Camera(hSize, vSize, fieldOfView);
+
+            Assert.AreEqual(hSize, camera.HSize);
+            Assert.AreEqual(vSize, camera.VSize);
+            Assert.AreEqual(fieldOfView, camera.FieldOfView);
+            Assert.AreEqual(Matrix.IdentityMatrix, camera.Transform);
+        }
+
+        [Test]
+        public void PixelSizeForHorizontalCanvas()
+        {
+
+            var camera = new Camera(200, 125, Math.PI/2);
+
+            Assert.IsTrue(DoubleUtils.DoubleEquals(0.01, camera.PixelSize));
+        }
+
+        [Test]
+        public void PixelSizeForVerticalCanvas()
+        {
+
+            var camera = new Camera(125, 200, Math.PI / 2);
+
+            Assert.IsTrue(DoubleUtils.DoubleEquals(0.01, camera.PixelSize));
+        }
+
+        [Test]
+        public void CreateRayThroughCentreOfCanvas()
+        {
+            var camera = new Camera(201, 101, Math.PI / 2);
+
+            var ray = camera.RayForPixel(100, 50);
+
+            Assert.AreEqual(new Point(0,0,0), ray.Origin);
+            Assert.AreEqual(new Vector(0, 0, -1), ray.Direction);
+        }
+
+        [Test]
+        public void CreateRayThroughCornerOfCanvas()
+        {
+            var camera = new Camera(201, 101, Math.PI / 2);
+
+            var ray = camera.RayForPixel(0, 0);
+
+            Assert.AreEqual(new Point(0, 0, 0), ray.Origin);
+            Assert.AreEqual(new Vector(0.66519, 0.33259, -0.66851), ray.Direction);
+        }
+
+        [Test]
+        public void CreateRayWhenCameraTransformed()
+        {
+            var camera = new Camera(201, 101, Math.PI / 2);
+            camera.Transform = Matrix.RotationY(Math.PI/4) * Matrix.Translation(0,-2,5);
+
+            var ray = camera.RayForPixel(100, 50);
+
+            Assert.AreEqual(new Point(0, 2, -5), ray.Origin);
+            Assert.AreEqual(new Vector(Math.Sqrt(2) / 2, 0, -(Math.Sqrt(2) / 2)), ray.Direction);
+        }
+
+        [Test]
+        public void RenderWorldWithCamera()
+        {
+            var world = World.DefaultWorld;
+            var camera = new Camera(11,11,Math.PI/2);
+
+            var from = new Point(0,0,-5);
+            var to = new Point(0,0,0);
+            var up = new Vector(0,1,0);
+
+            camera.Transform = Matrix.ViewTransform(from,to, up);
+            var image = camera.Render(world);
+
+            Assert.AreEqual(new Colour(0.38066, 0.47583, 0.2855), image.GetPixel(5,5));
+
+        }
+    }
+}
---
+++ RayTracerChallenge/RayTracer/Camera.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RayTracer
+{
+    public class Camera
+    {
+        public int HSize { get; private set; }
+        public int VSize { get; private set; }
+        public double FieldOfView { get; private set; }
+        public Matrix Transform { get; set; }
+
+        public double HalfWidth { get; private set; }
+
+        public double HalfHeight { get; private set; }
+
+        public double PixelSize { get; private set; }
+
+        public Camera(int hSize, int vSize, double fieldOfView)
+        {
+            HSize = hSize;
+            VSize = vSize;
+            FieldOfView = fieldOfView;
+            Transform = Matrix.IdentityMatrix;
+
+
+            var halfView = Math.Tan(FieldOfView / 2);
+            var aspect = CalcAspect(HSize, VSize);
+
+            if (aspect >= 1)
+            {
+                this.HalfWidth = halfView;
+                this.HalfHeight = halfView / aspect;
+            }
+            else
+            {
+                this.HalfWidth = halfView * aspect;
+                this.HalfHeight = halfView;
+            }
+
+            PixelSize = (HalfWidth * 2) / hSize;
+        }
+
+        private static double CalcAspect(double hSize, double vSize)
+        {
+            return hSize / vSize;
+        }
+
+        public Ray RayForPixel(int px, int py)
+        {
+            var xOffset = (px + 0.5) * PixelSize;
+            var yOffset = (py + 0.5) * PixelSize;
+
+            var worldX = HalfWidth - xOffset;
+            var worldY = HalfHeight - yOffset;
+
+            var pixel = Transform.Inverse() * new Point(worldX, worldY, -1);
+            var origin = Transform.Inverse() * new Point(0, 0, 0);
+            var direction = pixel.Subtract(origin).Normalize();
+
+            return new Ray(origin.ToPoint(), direction.ToVector());
+        }
+
+        public Canvas Render(World world)
+        {
+            var image = new Canvas(HSize,VSize);
+
+            for (var y = 0; y < VSize; y++)
+            {
+                for (var x = 0; x < HSize; x++)
+                {
+                    var ray = RayForPixel(x, y);
+                    var colour = world.ColourAt(ray);
+                    image.SetPixel(x, y, colour);
+                }
+            }
+
+            return image;
+        }
+    }
+}
