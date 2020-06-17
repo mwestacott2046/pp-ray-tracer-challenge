5fa532737f13a37aae304de1cd6f6611ff90e84e
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2020-06-17 22:17:32 +0100

    Add RefractionConstants file

---
+++ RayTracerChallenge/RayTracer/RefractionConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace RayTracer
+{
+    public class RefractionConstants
+    {
+        public const double Vacuum = 1.0;
+        public const double Air = 1.00029;
+        public const double Water = 1.333;
+        public const double Glass = 1.52;
+        public const double Diamond = 2.417;
+    }
+}
