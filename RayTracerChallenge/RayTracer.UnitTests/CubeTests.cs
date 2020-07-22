fe87a841bf6c6fd5214c00dfd6f690f558d2113c
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2020-07-22 14:48:15 +0100

    Implement Cube objects

---
+++ RayTracerChallenge/RayTracer.UnitTests/CubeTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using RayTracer.Shapes;
+
+namespace RayTracer.UnitTests
+{
+    [TestFixture]
+    public class CubeTests
+    {
+        // ReSharper disable once InconsistentNaming
+        public static object[] CubeIntersectsCases =
+        {
+            new object[] {"+x", new Point(5, 0.5, 0), new Vector(-1, 0, 0), 4, 6},
+            new object[] {"-x", new Point(-5, 0.5, 0), new Vector(1, 0, 0), 4, 6},
+            new object[] {"+y", new Point(0.5, 5, 0), new Vector(0, -1, 0), 4, 6},
+            new object[] {"-y", new Point(0.5, -5, 0), new Vector(0, 1, 0), 4, 6},
+            new object[] {"+z", new Point(0.5, 0, 5), new Vector(0, 0, -1), 4, 6},
+            new object[] {"-z", new Point(0.5, 0, -5), new Vector(0, 0, 1), 4, 6},
+            new object[] {"inside", new Point(0, 0.5, 0), new Vector(0, 0, 1), -1, 1},
+        };
+
+
+        [TestCaseSource(nameof(CubeIntersectsCases))]
+        public void RayIntersectsCube(string desc ,Point origin, Vector direction, int t1, int t2)
+        {
+            var c = new Cube();
+            var ray = new Ray(origin, direction);
+
+            var xs = c.Intersects(ray);
+            
+            Assert.AreEqual(2, xs.Length);
+            Assert.AreEqual(t1,xs[0].T);
+            Assert.AreEqual(t2, xs[1].T);
+        }
+
+
+
+        public static object[] CubeMissesCases =
+        {
+            new object[] { new Point(-2, 0, 0),  new Vector(0.2673, 0.5345, 0.8018) },
+            new object[] { new Point(0, -2, 0),  new Vector(0.8018, 0.2673, 0.5345) },
+            new object[] { new Point(0, 0, -2),  new Vector(0.5345, 0.8018, 0.2673) },
+            new object[] { new Point(2, 0, 2),  new Vector(0, 0, -1) },
+            new object[] { new Point(0, 2, 2),  new Vector(0, -1, 0) },
+            new object[] { new Point(2, 2, 0),  new Vector(-1, 0, 0) },
+        };
+
+
+        [TestCaseSource(nameof(CubeMissesCases))]
+        public void RayMissesCube(Point origin, Vector direction)
+        {
+            var c = new Cube();
+            var ray = new Ray(origin, direction);
+
+            var xs = c.Intersects(ray);
+
+            Assert.AreEqual(0, xs.Length);
+        }
+
+        public static object[] SurfaceNormalCases =
+        {
+            new object[] {new Point(1, 0.5, -0.8), new Vector(1, 0, 0)},
+            new object[] {new Point(-1, -0.2, 0.9), new Vector(-1, 0, 0)},
+            new object[] {new Point(-0.4, 1, -0.1), new Vector(0, 1, 0)},
+            new object[] {new Point(0.3, -1, -0.7), new Vector(0, -1, 0)},
+            new object[] {new Point(-0.6, 0.3, 1), new Vector(0, 0, 1)},
+            new object[] {new Point(0.4, 0.4, -1), new Vector(0, 0, -1)},
+            new object[] {new Point(1, 1, 1), new Vector(1, 0, 0)},
+            new object[] {new Point(-1, -1, -1), new Vector(-1, 0, 0)},
+        };
+
+        [TestCaseSource(nameof(SurfaceNormalCases))]
+        public void NormalOnSurfaceOfCube(Point point, Vector expectedNormal)
+        {
+            var c = new Cube();
+            var normal = c.NormalAt(point);
+
+            Assert.AreEqual(expectedNormal, normal);
+
+        }
+
+
+    }
+}
---
+++ RayTracerChallenge/RayTracer/Shapes/Cube.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RayTracer.Shapes
+{
+    public class Cube: AbstractShape
+    {
+        private struct MinMax
+        {
+            public double Min { get;}
+            public double Max { get;}
+
+            public MinMax(double min, double max)
+            {
+                this.Min = min;
+                this.Max = max;
+            }
+        }
+
+        protected override Intersection[] LocalIntersects(Ray localRay)
+        {
+            var xtMinMax = CheckAxis(localRay.Origin.X, localRay.Direction.X);
+            var ytMinMax = CheckAxis(localRay.Origin.Y, localRay.Direction.Y);
+            var ztMinMax = CheckAxis(localRay.Origin.Z, localRay.Direction.Z);
+
+            var tMin = new double[] {xtMinMax.Min,ytMinMax.Min, ztMinMax.Min}.Max();
+            var tMax = new double[] { xtMinMax.Max, ytMinMax.Max, ztMinMax.Max }.Min();
+
+            if (tMin > tMax)
+            {
+                return new Intersection[]{};
+            }
+
+            return new Intersection[] {new Intersection(tMin, this), new Intersection(tMax, this) };
+        }
+
+        private MinMax CheckAxis(double origin, double direction)
+        {
+            var tMinNumerator = (-1 -origin);
+            var tMaxNumerator = (1-origin);
+
+            double tMin;
+            double tMax;
+
+            if (Math.Abs(direction) >= DoubleUtils.Epsilon)
+            {
+                tMin = tMinNumerator / direction;
+                tMax = tMaxNumerator / direction;
+            }
+            else
+            {
+                tMin = tMinNumerator * double.PositiveInfinity;
+                tMax = tMaxNumerator * double.PositiveInfinity;
+            }
+
+            if (tMin > tMax)
+            {
+                (tMax, tMin) = (tMin, tMax);
+            }
+
+            return new MinMax(tMin, tMax);
+        }
+
+        protected override Vector LocalNormalAt(Point localPoint)
+        {
+            var maxC = new double[] {Math.Abs(localPoint.X), Math.Abs(localPoint.Y), Math.Abs(localPoint.Z)}.Max();
+
+            if (DoubleUtils.DoubleEquals(Math.Abs(localPoint.X),maxC))
+            {
+                return new Vector(localPoint.X,0,0);
+            }
+            else if (DoubleUtils.DoubleEquals(Math.Abs(localPoint.Y), maxC))
+            {
+                return new Vector( 0, localPoint.Y,0);
+            }
+
+            return new Vector(0,0,localPoint.Z);
+        }
+    }
+}
