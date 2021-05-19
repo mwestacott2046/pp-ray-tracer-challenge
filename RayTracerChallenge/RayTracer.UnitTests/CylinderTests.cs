c14440fc70eab95c4c7da448e66efe850a6a4438
Martin Westacott <28762670+mwestacott2046@users.noreply.github.com>
2021-05-19 20:04:09 +0100

    Hollow Cylinders

---
+++ RayTracerChallenge/RayTracer.UnitTests/CylinderTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using NUnit.Framework;
+using RayTracer.Shapes;
+
+namespace RayTracer.UnitTests
+{
+    [TestFixture]
+    public class CylinderTests
+    {
+        public static object[] RayMissesCylinderCases =
+        {
+            new object[] {new Point(1, 0, 0), new Vector(0, 1, 0)},
+            new object[] {new Point(0, 0, 0), new Vector(0, 1, 0)},
+            new object[] {new Point(0, 0, -5), new Vector(1, 1, 1)},
+        };
+
+        [TestCaseSource(nameof(RayMissesCylinderCases))]
+        public void RayMissesCylinder(Point origin, Vector direction)
+        {
+            var cylinder = new Cylinder();
+            var dir = direction.Normalize().ToVector();
+            var r = new Ray(origin, dir);
+
+            var xs = cylinder.Intersects(r);
+            Assert.AreEqual(0, xs.Length);
+        }
+
+
+        public static object[] RayHitsCylinderCases =
+        {
+            new object[] {new Point(1, 0, -5), new Vector(0, 0, 1), 5.0, 5.0},
+            new object[] {new Point(0, 0, -5), new Vector(0, 0, 1), 4.0, 6.0},
+            new object[] {new Point(0.5, 0, -5), new Vector(0.1, 1, 1), 6.80798, 7.08872},
+
+        };
+
+        [TestCaseSource(nameof(RayHitsCylinderCases))]
+        public void RayHitsCylinder(Point origin, Vector direction, double t0, double t1)
+        {
+            var cylinder = new Cylinder();
+            var dir = direction.Normalize().ToVector();
+            var r = new Ray(origin, dir);
+
+            var xs = cylinder.Intersects(r);
+            Assert.AreEqual(2, xs.Length);
+            Assert.True(DoubleUtils.DoubleEquals(  t0,xs[0].T));
+            Assert.True(DoubleUtils.DoubleEquals( t1, xs[1].T));
+
+        }
+
+
+        public static object[] NormalOnCylinderCases =
+        {
+            new object[] {new Point(1, 0, 0), new Vector(1, 0, 0)},
+            new object[] {new Point(0, 5, -1), new Vector(0, 0, -1)},
+            new object[] {new Point(0, -2, 1), new Vector(0, 0, 1)},
+            new object[] {new Point(-1, 1, 0), new Vector(-1, 0, 0)},
+        };
+
+
+        [TestCaseSource(nameof(NormalOnCylinderCases))]
+        public void NormalOnCylinder(Point point, Vector normal)
+        {
+            var cyl = new Cylinder();
+            var n = cyl.NormalAt(point);
+            Assert.AreEqual(normal,n);
+        }
+
+        [Test]
+        public void DefaultMinMaxY ()
+        {
+            var cyl = new Cylinder();
+
+            Assert.AreEqual(Double.MinValue, cyl.Minimum);
+            Assert.AreEqual(Double.MaxValue, cyl.Maximum);
+        }
+
+
+        /*
+        |   | point             | direction         | count |
+​ 	    | 1 | point(0, 1.5, 0)  | vector(0.1, 1, 0) | 0     |
+​ 	    | 2 | point(0, 3, -5)   | vector(0, 0, 1)   | 0     |
+​ 	    | 3 | point(0, 0, -5)   | vector(0, 0, 1)   | 0     |
+​ 	    | 4 | point(0, 2, -5)   | vector(0, 0, 1)   | 0     |
+​ 	    | 5 | point(0, 1, -5)   | vector(0, 0, 1)   | 0     |
+​ 	    | 6 | point(0, 1.5, -2) | vector(0, 0, 1)   | 2     |
+         */
+        public static object[] IntersectingConstrainedCylinderCases =
+        {
+            new object[] {new Point(0, 1.5, 0), new Vector(0.1, 1, 0), 0},
+            new object[] {new Point(0, 3, -5), new Vector(0, 0, 1), 0},
+            new object[] {new Point(0, 0, -5), new Vector(0, 0, 1), 0},
+            new object[] {new Point(0, 2, -5), new Vector(0, 0, 1), 0},
+            new object[] {new Point(0, 1, -5), new Vector(0, 0, 1), 0},
+            new object[] {new Point(0, 1.5, -2), new Vector(0, 0, 1), 2},
+        };
+
+        [TestCaseSource(nameof(IntersectingConstrainedCylinderCases))]
+        public void IntersectingConstrainedCylinder(Point point, Vector direction, int expectedCount)
+        {
+            var cylinder = new Cylinder {Maximum = 2, Minimum = 1};
+
+            var dir = direction.Normalize().ToVector();
+            var r = new Ray(point, dir);
+
+            var xs = cylinder.Intersects(r);
+            Assert.AreEqual(expectedCount, xs.Length);
+        }
+
+        [Test]
+        public void DefaultClosedValue()
+        {
+            var cyl = new Cylinder();
+            Assert.False(cyl.Closed);
+        }
+    }
+}
---
+++ RayTracerChallenge/RayTracer/Shapes/Cylinder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RayTracer.Shapes
+{
+    public class Cylinder : AbstractShape
+    {
+        public Cylinder()
+        {
+            Minimum = double.MinValue;
+            Maximum = double.MaxValue;
+            Closed = false;
+        }
+
+        protected override Intersection[] LocalIntersects(Ray localRay)
+        {
+            var a =  Math.Pow(localRay.Direction.X,2) + Math.Pow(localRay.Direction.Z,2);
+            // Is parallel to cylinder
+            if (DoubleUtils.DoubleEquals(a, 0))
+            {
+                return new Intersection[] {};
+            }
+
+            var b = 2 * localRay.Origin.X * localRay.Direction.X +
+                    2 * localRay.Origin.Z * localRay.Direction.Z;
+            var c = Math.Pow(localRay.Origin.X, 2) + Math.Pow(localRay.Origin.Z, 2) -1;
+
+            var disc = Math.Pow(b, 2) - 4 * a * c;
+            
+            //if Ray has missed
+            if (disc < 0)
+            {
+                return new Intersection[] { };
+            }
+
+            var t0 = (-b - Math.Sqrt(disc)) / (2 * a);
+            var t1 = (-b + Math.Sqrt(disc)) / (2 * a);
+
+            if (t0 > t1)
+            {
+                (t0, t1) = (t1, t0);
+            }
+
+            var xs = new List<Intersection>();
+
+            var y0 = localRay.Origin.Y + t0 * localRay.Direction.Y;
+            if (this.Minimum < y0 && y0 < this.Maximum)
+            {
+                xs.Add(new Intersection(t0,this));
+            }
+
+            var y1 = localRay.Origin.Y + t1 * localRay.Direction.Y;
+            if (this.Minimum < y1 && y1 < this.Maximum)
+            {
+                xs.Add(new Intersection(t1, this));
+            }
+
+            return xs.ToArray();
+        }
+
+        protected override Vector LocalNormalAt(Point localPoint)
+        {
+            return new Vector(localPoint.X, 0, localPoint.Z);
+        }
+
+        public double Minimum { get; set; }
+        public double Maximum { get; set; }
+        public bool Closed { get; set; }
+    }
+}
